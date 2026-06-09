826a64cf6c83e456c0dc444a5e8ce5d28088af0f
Hridesh Devkota <hridevkota11@gmail.com>
2026-06-09 23:19:49 +0545

    Repairment for paper begun

---
+++ AUTOMATED-KYC-VERIFICATION-FOR-NEPALI-CITIZENSHIP-USING-DEEP-LEARNING-AND-FACIAL-BIOMETRIC-AUTHENTIC/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from automated-kyc-verification-for-nepali-citizenship-using-deep-learning-and-facial-biometric-authentic!")
+
+
+if __name__ == "__main__":
+    main()
